96e323d8f5b78c5939f67147d122d2f55e130380
kodyclemens <kody.clemens@gmail.com>
2019-01-23 18:12:28 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'users#new'
+  get '/signin', to: 'sessions#new'
+  post '/ride', to: 'rides#ride'
+  delete '/logout', to: 'sessions#destroy'
   
+  resources :sessions, only: [:create, :destroy]
+  resources :users
+  resources :attractions
+
 end
