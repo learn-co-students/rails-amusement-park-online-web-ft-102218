b2c9975ed3e5d145897786cdc56d4d1d3767820d
endotnick <endotnick@gmail.com>
2019-01-26 16:19:27 -0800

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,33 @@
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    eligible, message = rider_check
+    update_user if eligible
+    message
+  end
+
+  def rider_check
+    eligible = true
+    message = 'Sorry.'
+
+    if user.tickets < attraction.tickets
+      eligible = false
+      message += " You do not have enough tickets to ride the #{attraction.name}."
+    end
+    if user.height < attraction.min_height
+      eligible = false
+      message += " You are not tall enough to ride the #{attraction.name}."
+    end
+    message = "Thanks for riding the #{attraction.name}!" if eligible
+    [eligible, message]
+  end
+
+  def update_user
+    user.tickets -= attraction.tickets
+    user.nausea += attraction.nausea_rating
+    user.happiness += attraction.happiness_rating
+    user.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  
+  root 'welcome#index'
+  resources :users, only: [:index, :new, :show, :create, :edit]
+  resources :attractions, only: [:index, :new, :show, :create, :edit, :update]
+  get '/signin' => 'sessions#new'
+  post '/sessions/create'
+  post '/logout' => 'sessions#destroy'
+  post '/rides/new'
 end
